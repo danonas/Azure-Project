9ba868ca28b3bd714c221ad540cf4bad96676634
Vasyl <120543770+kofeart@users.noreply.github.com>
2023-03-28 17:14:53 -0500

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-
-# Define the Terraform output to return the MySQL instance hostname and database name:
-output "mysql_hostname" {
-  value = azurerm_mysql_server.azureproject_server.fqdn
-}
-
-output "mysql_database_name" {
-  value = azurerm_mysql_database.wordpress_db.name
-}
