22106daef2f3518c2797391a952ed6bad131a62e
Mazlumyavuz <120551588+Mazlumyavuz@users.noreply.github.com>
2023-04-04 16:18:40 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "wordpress" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
 resource "azurerm_virtual_network" "wordpress" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
