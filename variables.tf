dfc720bfd9584a4bad03ebcdf8aaf59d26eeeef7
Mazlumyavuz <120551588+Mazlumyavuz@users.noreply.github.com>
2023-04-04 16:27:58 -0500

    added variables

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "location" {
+  description = "The location where resources will be created"
+  type        = string
+  default     = ""
+}
+
+
+
+#variable mysql
+variable "database_admin_login" {
+  default = "mysqladminun"
+}
+
+variable "database_admin_password" {
+  default = "W0rdpr3ss@p4ss"
+}
+
+#Mysql database name
+variable "dbname" {
+  default = "db-wordpress"
+}
+
+#lb http rule and lb_probe
+variable "application_port" {
+  default = 80
+}
+
+
+variable "ssh_port" {
+  default = 22
+}
+
+#VM username and password
+variable "admin_username" {
+  default = "wordpress"
+}
+
+variable "admin_password" {
+  description = "Default password for admin account"
+  default     = "W0rdpr3ss@p4ss"
+}
