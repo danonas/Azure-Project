9e19684842805f71bd5e5cb685d614ec46b7d346
danonas <120547200+danonas@users.noreply.github.com>
2023-04-04 22:33:35 -0500

    removed backend

--- envs/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-eastus"
-    storage_account_name = "cs21003200285d9a13c"
-    container_name       = "tfstate"
-    key                  = "Azure-Project.terraform.tfstate"
-  }
-}
